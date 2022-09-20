7a5b1d5af51dc5901b26169457a44b862ad126fc
Satwika Vemuri <satwika@beatclub.com>
2022-09-20 14:10:54 -0400

    typescript app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+//src/App.vue
+//Typescript code
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  components:{
+    HelloWorld,
+  },
+  data(){
+    return{
+      title:"welcome to my app",
+      list: [
+    {
+      name: 'popoolatopzy',
+      age: '26'
+    },
+    {
+      name: 'Preetish',
+      age: '26'
+    },
+    {
+      name: 'John',
+      age: '30'
+    }
+  ]
+    }
+  }
+})
+</script>
