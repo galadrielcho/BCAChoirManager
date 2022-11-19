6e4cfcbd0fde531923a61a4a50286b42a86fb93a
Lorien Cho <42721802+loriencho@users.noreply.github.com>
2022-11-19 18:19:30 -0500

    setup routing, created navbar, home component (did not add all files previously)

--- frontend/bca-choir-manager/src/app/app.module.ts
+++ frontend/bca-choir-manager/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './navigation/header/header.component';
+import { RoutingModule } from './routing/routing.module';
+import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RoutingModule,
+    MaterialModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
