4aed62c0e375daa9102cb2d8ee0457d8674be3ab
Lorien Cho <42721802+loriencho@users.noreply.github.com>
2022-12-14 16:01:07 -0500

    Completed missing import statements from merge

--- frontend/bca-choir-manager/src/app/material/material.module.ts
+++ frontend/bca-choir-manager/src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [],
