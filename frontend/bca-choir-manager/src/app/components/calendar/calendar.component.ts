60df2174a73eef6462a44e32d456579f997bb3aa
Lorien Cho <42721802+loriencho@users.noreply.github.com>
2022-12-15 14:14:29 -0500

    calendar styling

--- frontend/bca-choir-manager/src/app/components/calendar/calendar.component.ts
+++ frontend/bca-choir-manager/src/app/components/calendar/calendar.component.ts
@@ -9,20 +9,39 @@ import { CalendarService } from '../../services/calendar-service/calendar.servic
 export class CalendarComponent implements OnInit{
   public weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];  
   public calendarMonth : number[][];
+  public calendarTitle : string;
 
   ngOnInit(): void{
     this.calendarService.setDate(new Date());
-    console.log("Last current:");
-    console.log(this.calendarService.getCalendarMonthArray());
+  }
 
+  setMonthForward(): void{
+    console.log("forward");
+    this.calendarService.setMonthForward();
+    this.updateCalendar(this.getDate());
+
+  }
+
+  setMonthBackward(): void{    
+    console.log("backward");
+    this.calendarService.setMonthBackward();
+    this.updateCalendar(this.getDate());
+  }
+
+  getDate() : Date {
+    return this.calendarService.getDate();
   }
 
   updateCalendar(date : Date): void{
     this.calendarService.setDate(date);
     this.calendarMonth = this.calendarService.getCalendarMonthArray();
+    this.calendarTitle = this.getDate().toLocaleString('default', { month: 'long' }) + " "+ this.getDate().getFullYear();
+
   }
   
   constructor(private calendarService : CalendarService){
     this.calendarMonth = this.calendarService.getCalendarMonthArray();
+    this.calendarTitle = this.getDate().toLocaleString('default', { month: 'long' }) + " "+ this.getDate().getFullYear();
+
   }
 }
