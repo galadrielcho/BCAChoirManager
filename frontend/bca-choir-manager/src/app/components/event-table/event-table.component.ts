ccd23ba99733aef853a1f89ac01d2bcf57748f6c
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2023-02-04 15:04:40 -0500

    added event searching

--- frontend/bca-choir-manager/src/app/components/event-table/event-table.component.ts
+++ frontend/bca-choir-manager/src/app/components/event-table/event-table.component.ts
@@ -1,11 +1,9 @@
 import { AfterViewInit, Component, ElementRef, ViewChild , Renderer2} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { MatTable } from '@angular/material/table';
-import { EventTableDataSource, EventTableItem } from './event-table-datasource';
-import { RosterService} from '../../services/roster-service/roster.service';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import { RosterUpdateComponent } from 'src/app/roster-update/roster-update.component';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
+// import { EventTableDataSource, EventTableItem } from './event-table-datasource';
+import {MatDialog,} from '@angular/material/dialog';
 import { EventService } from 'src/app/services/event-service/event.service';
 import { EventData } from 'src/app/models/event-data.model';
 @Component({
@@ -15,62 +13,50 @@ import { EventData } from 'src/app/models/event-data.model';
   styleUrls: ['./event-table.component.css']
 })
 
-export class EventTableComponent implements AfterViewInit {
+export class EventTableComponent {
+  displayedColumns = ['event_name', 'type', 'start_time', 'end_time', 'expand'];
+  private events : EventData[] = [];
+  dataSource: MatTableDataSource<EventData> = new MatTableDataSource<EventData>([]);
+
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<EventTableItem>;
-  dataSource: EventTableDataSource | undefined;
   eventService: EventService;
-  dialog: MatDialog;
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['event_name', 'type', 'start_time', 'end_time', 'expand'];
-  
+  constructor(private es: EventService, private md: MatDialog) { 
+    this.eventService = es;
+    this.eventService.getEvents().subscribe({
+      next: data => {
+        this.events = data.events;
+        this.setupTable();
+
+      }      
+    }); 
+    
+
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  setupTable() {
+    this.dataSource = new MatTableDataSource(this.events);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+
+    console.log(this.dataSource.data);
+
+  }
+
   expandEvent(event_name: string, start_time: string){
 
 
   }
 
-
-  constructor(private es: EventService, private md: MatDialog) { 
-    
-    this.eventService = es;
-    this.dialog = md;
-    var events: EventTableItem[] = [];
-    
-    let event : EventData;
-
-
-    this.eventService.getEvents().subscribe({
-      next: data => {
-        console.log(data);
-
-        // for(let i = 0; i < data.events.length; i++){
-        //   event = data[i];
-          
-        //   events.push({event_name: data.events[i][0], 
-        //               type: data.events[i][1],
-        //               start_time: data.events[i][2],
-        //               end_time: data.events[i][3]
-        //   }
-                      
-        //   );
-          
-        // }
-        this.dataSource = new EventTableDataSource(data.events);
-        this.dataSource.sort = this.sort;
-        this.dataSource.paginator = this.paginator;
-        this.table.dataSource = this.dataSource;   
-      }      
-    }); 
-    
-    
-  
-    
-    
-  }
-
-  ngAfterViewInit(): void {
+  applyFilter(event: Event) {
+    if (this.dataSource) {
+      const filterValue = (event.target as HTMLInputElement).value;
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
   }
 
 }
