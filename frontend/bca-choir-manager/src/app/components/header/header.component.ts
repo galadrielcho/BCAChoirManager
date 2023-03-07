beafb337bdd46375d04c40946c7933cbe0ba9182
satwika-vemuri <satwika.vemuri@gmail.com>
2023-03-07 15:18:31 -0500

    progress, but need to work on asyncs for admin

--- frontend/bca-choir-manager/src/app/components/header/header.component.ts
+++ frontend/bca-choir-manager/src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { SignUpService } from 'src/app/services/sign-up-service/sign-up.service';
 import { AuthService } from '@auth0/auth0-angular';
+import { AuthenticationService } from 'src/app/services/authentication-service/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -11,14 +12,19 @@ import { AuthService } from '@auth0/auth0-angular';
 export class HeaderComponent implements OnInit {
   dialog: MatDialog;
   signUpService: SignUpService
+  authenticationService: AuthenticationService
 
-  constructor(private md: MatDialog, private sus: SignUpService, public auth: AuthService) { 
+  constructor(private md: MatDialog, private sus: SignUpService, public auth: AuthService, public as: AuthenticationService) { 
     this.dialog = md;
     this.signUpService = sus;
+    this.authenticationService = as;
   }
 
   isAdmin(email: string|undefined){
-    return false;
+    this.authenticationService.isAdmin(email).then(res => {
+      //WTH SHOULD I DO HERE
+    })
+    
   }
 
   ngOnInit(): void {
