319379f28891a09fc75016ae6cdaadc0ae877464
Galadriel Cho <42721783+galadrielcho@users.noreply.github.com>
2023-03-24 14:02:10 -0400

    fixing direct url routing

--- backend/routes/utils.js
+++ backend/routes/utils.js
@@ -2,6 +2,16 @@ var database = require('../db');
 const router = require('express').Router();
 
 module.exports = function () {
+    /*  "/api/status"
+    *   GET: Get server status
+    */
+   
+    router.get("/", function (req, res) {
+        console.log("test");
+        folderDir = './../frontend/bca-choir-manager/dist/bca-choir-manager'
+        res.sendFile(path.join(folderDir, '/index.html'));
+    });
+    
 
     /*  "/api/status"
     *   GET: Get server status
