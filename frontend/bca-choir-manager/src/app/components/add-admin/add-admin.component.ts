9b946a08e985039277adab8a7ec4f83a8eebc893
satwika-vemuri <satwika.vemuri@gmail.com>
2023-03-28 13:58:02 -0400

    admin popup

--- frontend/bca-choir-manager/src/app/components/add-admin/add-admin.component.ts
+++ frontend/bca-choir-manager/src/app/components/add-admin/add-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AccountService } from 'src/app/services/account-service/account.service';
 
 @Component({
   selector: 'app-add-admin',
@@ -6,9 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-admin.component.css']
 })
 export class AddAdminComponent {
+  firstname = "";
+  lastname = "";
+  email = "";
+
+  constructor(private service: AccountService){
+
+  }
 
   submit(){
-    
+    this.service.addAdmin([this.email, this.firstname, this.lastname]);
   }
 
 }
