7b04a77a7eb4bed62701ae9cb0af36b6630ca2ab
galadrielcho <42721783+galadrielcho@users.noreply.github.com>
2023-04-26 23:41:31 -0400

    roster related queries converted to prepared statements

--- routes/roster.js
+++ routes/roster.js
@@ -10,15 +10,18 @@ module.exports = function () {
  *      voice part number, choir type, grad_year, email
  */
 
-    router.get("/api/get-roster", function (req, res) {
+  router.get("/api/get-roster", function (req, res) {
     sql = `CALL getStudentRoster();`
-    database.query(sql, function(err, roster, fields) 
-    {
-    if (err) throw err;
-  
-    let result = Object.values(JSON.parse(JSON.stringify(roster[0])));
-   
-    res.send({roster: result});
+    database.execute(
+      "CALL getStudentRoster()",
+      [],
+      function(err, roster, fields) {
+        if (err) throw err;
+        if (Object.keys(roster).length === 0) {
+          res.send({roster: results});
+        }
+        let result = Object.values(JSON.parse(JSON.stringify(roster[0])));
+        res.send({roster: result});
     });
   });
   
@@ -29,49 +32,42 @@ module.exports = function () {
 *      voice part number, choir type, grad year
 */
   
-  router.get("/api/get-student/:email", function (req, res) {
-    sql = `CALL getStudent("${req.params.email}");`
-  
-    database.query(sql, function(err, student, fields) 
-    
-    {
-    let result = Object.values(JSON.parse(JSON.stringify(student[0])))[0];
-    if (err) throw err;
-    res.send({details: result});
-    });
+  router.get("/api/get-student/:email", function (req, res) {  
+    database.execute(
+      "CALL getStudent(?)",
+      [req.params.email],
+      function(err, student, fields) {
+        if (err) throw err;
+        if (Object.keys(student).length === 0) res.send({details: results});
+
+        let result = Object.values(JSON.parse(JSON.stringify(student[0])))[0];
+        res.send({details: result});
+      });
   });
 
 
 /*  "/api/roster-update/"
 *   POST: Updates the data of a student account.
-*   Updates the following infromation:
-*   email, first name, last name, pronouns,
-    voicepart name, voicepart number, choir type,
-    graduation year
+*         Updates the following infromation:
+*         email, first name, last name, pronouns,
+*         voicepart name, voicepart number, choir type,
+*         graduation year
 */
 
   router.post("/api/roster-update", function (req, res) {
     let student = req.body;
-  
-    sql = `CALL updateStudent("${student.email}",`
-      + `"${student.first_name}",`
-      + `"${student.last_name}",`
-      + `"${student.pronouns}",`
-      + `"${student.voicepart_name}",`
-      + `${student.number},`
-      + `"${student.choir_name}",`
-      + `${student.grad_year});`
-  
-      database.query(sql, function(err) 
-    {
-    if (err) throw err;
-     });
-  
-  
+
+    database.execute(
+      "CALL updateStudent(?, ?, ?, ?, ?, ?, ?, ?)",
+      [student.email, student.first_name, student.last_name, student.pronouns,
+        student.voicepart_name, student.number, student.choir_name, student.grad_year],
+      function(err) 
+      {
+        if (err) throw err;
+    });
   });
 
-
   return router;
-  
+
 
 }
