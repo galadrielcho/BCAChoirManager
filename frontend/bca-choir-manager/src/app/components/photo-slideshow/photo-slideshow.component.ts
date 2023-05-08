c25a47844d28affda7e2dff1dc6539acd6850c2a
satwika-vemuri <satwika.vemuri@gmail.com>
2023-05-08 13:36:29 -0400

    photo slideshow preloading

--- frontend/bca-choir-manager/src/app/components/photo-slideshow/photo-slideshow.component.ts
+++ frontend/bca-choir-manager/src/app/components/photo-slideshow/photo-slideshow.component.ts
@@ -1,28 +1,38 @@
 import { Component, Input } from '@angular/core';
 import { SlideInterface } from './types/slides.interface';
-
 @Component({
   selector: 'app-photo-slideshow',
   templateUrl: './photo-slideshow.component.html',
   styleUrls: ['./photo-slideshow.component.css']
 })
 export class PhotoSlideshowComponent {
-  @Input() slides: SlideInterface[] = []
+  @Input() slides: SlideInterface[] = [];
 
   currentIndex: number = 0;
+  imageObjects: HTMLImageElement[] = [];
 
-  goToNext(): void{
+  constructor() {
+    // preload images in the background
+    this.slides.forEach((slide) => {
+      const img = new Image();
+      img.src = slide.url;
+      this.imageObjects.push(img);
+    });
+  }
+
+  goToNext(): void {
     const isLastSlide =  this.currentIndex === this.slides.length - 1;
     const newIndex = isLastSlide ? 0 : this.currentIndex + 1;
     this.currentIndex = newIndex;
   }
 
-  goToPrevious(): void{
+  goToPrevious(): void {
     const isFirstSlide = this.currentIndex === 0;
     const newIndex = isFirstSlide ? (this.slides.length - 1) : this.currentIndex - 1;
     this.currentIndex = newIndex;
   }
-  getCurrentSlideUrl(): string{
+
+  getCurrentSlideUrl(): string {
     return `url('${this.slides[this.currentIndex].url}')`;
   }
 }
