f1b5f8b16295306e3be2dcb629bb41e6a3de9d4f
galadrielcho <42721783+galadrielcho@users.noreply.github.com>
2023-05-16 22:07:48 -0400

    setting up accessing auth0 management api

--- auth.js
+++ auth.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
+
 const {expressjwt:jwt} = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 const cors = require('cors');
 const jwtAuthz = require('express-jwt-authz');
+var axios = require("axios").default;
+
+
+var AuthenticationClient = require('auth0').AuthenticationClient;
+
+var webAuth = new auth0.WebAuth({
+  domain:       process.env.AUTH0_DOMAIN,
+  clientID:     process.env.AUTH0_CLIENT_ID
+});
+
 
 const checkJwt = jwt({
     secret: jwksRsa.expressJwtSecret({
@@ -15,8 +27,38 @@ const checkJwt = jwt({
     audience: 'https://bcachoir.glitch.me/api/', //replace with your API's audience, available at Dashboard > APIs
     issuer: 'https://bca-choir-manager.us.auth0.com/',
     algorithms: [ 'RS256' ]
+      ) {
+          let t = req.headers.authorization.split(" ")[1];
+          currentJWT = t;
 });
   
   
 
-module.exports = {checkJwt, cors, jwtAuthz};
+module.exports = {checkJwt, cors, jwtAuthz};    // TO DO: add account deletion
+    throw error;
+  });
+
+}
+
+function getUser(token) {
+  webAuth.client.userInfo(token, function(err, user) {
+    if (err) throw error;
+    return user;
+  });
+}
+
+
+// Parse the URL and extract the Access Token
+webAuth.parseHash(window.location.hash, function(err, authResult) {
+  if (err) {
+    return console.log(err);
+  }
+  webAuth.client.userInfo(authResult.accessToken, function(err, user) {
+      // This method will make a request to the /userinfo endpoint
+      // and return the user object, which contains the user's information,
+      // similar to the response below.
+  });
+});
+
+
+module.exports = {checkJwt, cors, jwtAuthz, currentJWT, getManagementToken, getUser};
